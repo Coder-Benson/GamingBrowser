17cf5ec4b016e56295f3a91dad84bb6b66b830d4
Coder-Benson <161645562+Coder-Benson@users.noreply.github.com>
2024-02-29 18:32:36 -0500

    Add files via upload

---
+++ MyBrowser.js
@@ -0,0 +1,20 @@
+const dropdownBtn = document.querySelector(".dropdown__button");
+const dropdownMenu = document.querySelector(".dropdown__menu");
+
+dropdownBtn.addEventListener("click", () => {
+dropdownMenu.classList.toggle("hide");
+});
+
+const dropdownBtn2 = document.querySelector(".dropdown__button2");
+const dropdownMenu2 = document.querySelector(".dropdown__menu2");
+
+dropdownBtn2.addEventListener("click", () => {
+dropdownMenu2.classList.toggle("hide");
+});
+
+const dropdownBtn3 = document.querySelector(".dropdown__button3");
+const dropdownMenu3 = document.querySelector(".dropdown__menu3");
+
+dropdownBtn3.addEventListener("click", () => {
+dropdownMenu3.classList.toggle("hide");
+});
